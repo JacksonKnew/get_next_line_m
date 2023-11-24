08427c59414b3f57b29ed674a986d96c60bce623
JacksonKnew <paullestrat@hotmail.fr>
2023-11-24 13:07:32 +0100

    commentaires ajoutés

--- get_next_line.c
+++ get_next_line.c
@@ -50,36 +50,49 @@ char	*ft_read_to_left_str(int fd, char *left_str)
 	char	*buff;
 	int		rd_bytes;
 
+	// malloc le buffer
 	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buff)
 		return (NULL);
+
+	// On boucle sur les lectures du fichier tant qu'il n'y a pas de \n dans le buffer
 	rd_bytes = 1;
 	while (!ft_strchr(left_str, '\n') && rd_bytes != 0)
 	{
 		rd_bytes = read(fd, buff, BUFFER_SIZE);
+		// Si problème
 		if (rd_bytes == -1)
 		{
 			free(buff);
 			return (NULL);
 		}
+		// On ajoute le \0 quui n'est pas dans le buffer de base
 		buff[rd_bytes] = '\0';
+		// On join à ce qui a déja été lu
 		left_str = ft_strjoin(left_str, buff);
 	}
+	// Ne pas oublier de la free!
 	free(buff);
 	return (left_str);
 }
 
 char	*get_next_line(int fd)
 {
+	// line : ce qu'on va construire et renvoyer
 	char		*line;
+	// left_str correspond à ce qu'il restait au dernier appel de get_next_line après le \n dans le buffer
 	static char	*left_str;
 
+	// Si problème
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
+	// On fait autant de lectures que nécessaire pour avoir un \n
 	left_str = ft_read_to_left_str(fd, left_str);
 	if (!left_str)
 		return (NULL);
+	// maintenant qu'on sait qu'il y a un \n, on récupère jusqu'à lui
 	line = ft_get_line(left_str);
+	// On met dans la variable static ce qu'il y a après dans ce qui a été lu
 	left_str = ft_new_left_str(left_str);
 	return (line);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -47,14 +47,19 @@ size_t	ft_strlen(char *s)
 }
 
 char	*ft_strchr(char *s, int c)
-{
+{	
+	// On cherche si le char c est dans la chaine de caractères s
 	int	i;
 
 	i = 0;
+	// Cas pas de chaine
 	if (!s)
 		return (0);
+	// Cas caractère fin de string -> renvoie la taille de la string 
 	if (c == '\0')
 		return ((char *)&s[ft_strlen(s)]);
+	
+	// On boucle tant qu'on arrive pas à la fin
 	while (s[i] != '\0')
 	{
 		if (s[i] == (char) c)
@@ -70,16 +75,23 @@ char	*ft_strjoin(char *left_str, char *buff)
 	size_t	j;
 	char	*str;
 
+	// Si on a pas de left string -> on défini left_str = "";
 	if (!left_str)
 	{
 		left_str = (char *)malloc(1 * sizeof(char));
 		left_str[0] = '\0';
 	}
+	// Si y a rien dans les deux qu'est ce que tu fous la (!left_str redondant)
 	if (!left_str || !buff)
 		return (NULL);
+
+	// On malloc la taille des 2 str +1 (pour "\0")
 	str = malloc(sizeof(char) * ((ft_strlen(left_str) + ft_strlen(buff)) + 1));
+	// Si malloc échoue
 	if (str == NULL)
 		return (NULL);
+
+	// On ajoute 1 à 1 les caractères dans str depuis left_str puis depuis buffer
 	i = -1;
 	j = 0;
 	if (left_str)
@@ -87,6 +99,9 @@ char	*ft_strjoin(char *left_str, char *buff)
 			str[i] = left_str[i];
 	while (buff[j] != '\0')
 		str[i++] = buff[j++];
+
+	// On ajoute le "\0" à la fin et free left_str mais pas buffer qui sera réutilisé 
+	// buff est free dans ft_read_to_left_str
 	str[ft_strlen(left_str) + ft_strlen(buff)] = '\0';
 	free(left_str);
 	return (str);
@@ -94,23 +109,27 @@ char	*ft_strjoin(char *left_str, char *buff)
 
 char	*ft_get_line(char *left_str)
 {
+	// On sait qu'il y a un "\n" (grace à ft_str_chr) dans la ligne et on veut la récupérer
 	int		i;
 	char	*str;
 
 	i = 0;
 	if (!left_str[i])
 		return (NULL);
+	// Boucle pour compter l'indice du "\n" et malloc cette taille
 	while (left_str[i] && left_str[i] != '\n')
 		i++;
-	str = (char *)malloc(sizeof(char) * (i + 2));
-	if (!str)
+	str = (char *)malloc(sizeof(char) * (i + 2)); // malloc +2 pour "\n\0"
+	if (!str)  // si le malloc fail
 		return (NULL);
 	i = 0;
+	// Boucle pour remplir la str qui vient d'être malloc
 	while (left_str[i] && left_str[i] != '\n')
 	{
 		str[i] = left_str[i];
 		i++;
 	}
+	// On ajoute les les deux derniers caractères
 	if (left_str[i] == '\n')
 	{
 		str[i] = left_str[i];
@@ -122,25 +141,30 @@ char	*ft_get_line(char *left_str)
 
 char	*ft_new_left_str(char *left_str)
 {
+	// Enregistre ce qu'il reste du buffer après le \n
 	int		i;
 	int		j;
 	char	*str;
-
+	// On parcourt le buffer jusqu'au \n
 	i = 0;
 	while (left_str[i] && left_str[i] != '\n')
 		i++;
+	// On free s'il n'y a plus rien
 	if (!left_str[i])
 	{
 		free(left_str);
 		return (NULL);
 	}
+	// on malloc la taille restantedu buffer 
 	str = (char *)malloc(sizeof(char) * (ft_strlen(left_str) - i + 1));
 	if (!str)
 		return (NULL);
 	i++;
+	// On remplit le left_str avec la fin du buffer
 	j = 0;
 	while (left_str[i])
 		str[j++] = left_str[i++];
+	// On rajoute le \0
 	str[j] = '\0';
 	free(left_str);
 	return (str);
